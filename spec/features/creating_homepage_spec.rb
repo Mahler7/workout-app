7f35f45b4222fa709455711ba4f0918e7646f53a
Mahler7 <jjungstprogramming@gmail.com>
2019-05-19 12:38:50 -0500

    implement home page

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -5,20 +5,8 @@ RSpec.feature "A user visits the home page" do
   scenario "A user sees a link for Home" do
     visit "/"
     expect(page).to have_link("Home")
-  end
-
-  scenario "A user sees a link for Athletes Den" do
-    visit "/"
     expect(page).to have_link("Atheletes Den")
-  end
-
-  scenario "A user sees content for Workout Lounge" do
-    visit "/"
     expect(page).to have_content("Workout Lounge")
-  end
-
-  scenario "A user sees content for Show Off Your Workout" do
-    visit "/"
     expect(page).to have_content("Show Off Your Workout")
   end
 end
